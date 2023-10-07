238ea1b268d9d1dbeeab2837b5a9274976c8d9df
Sarah Dean <128323898+sarahadean@users.noreply.github.com>
2023-10-06 21:42:26 -0500

    import updates

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './components/App.jsx'
 import './index.css'
 import CssBaseline from '@mui/material/CssBaseline'
-import ThemeProvider from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material/styles'
 import theme from './theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
